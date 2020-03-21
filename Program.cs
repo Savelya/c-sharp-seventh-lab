e06601491e05e0c2a964d6ed2033185d268559e2
Savelya <savabezenkov@mail.ru>
2020-03-21 12:03:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace seventh_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string: ");
+            string text = Console.ReadLine();
+            Console.WriteLine(String.Concat(text.OrderBy(c => c)));
+        }
+    }
+}
